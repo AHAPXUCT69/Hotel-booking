f0911b93429d0d3b1b4ea27c2bcbd4e531f92049
AHAPXUCT69 <slastenin13@gmail.com>
2021-03-08 18:58:59 +0300

    Fix: fixed and set swiper slider

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
 const swiper = new Swiper('.swiper-container', {
   // Optional parameters
   loop: true,
+  grabCursor: true,
 
   // Navigation arrows
   navigation: {
     nextEl: '.slider-button--next',
     prevEl: '.slider-button--prev',
+  },
+
+// Keyboard control
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
   },
 });
