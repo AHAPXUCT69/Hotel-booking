6e87e4ad6ca85840dd3f2df52b1e6831756c7bd6
AHAPXUCT69 <slastenin13@gmail.com>
2021-03-11 23:06:33 +0300

    Create: added reviews slider in index.html & fixed hotel-slider classes

--- js/main.js
+++ js/main.js
@@ -1,12 +1,34 @@
-const swiper = new Swiper('.swiper-container', {
+// Hotel Slider
+const hotelSLider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
   grabCursor: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: '.slider-button--next',
-    prevEl: '.slider-button--prev',
+    nextEl: '.hotel-slider__button--next',
+    prevEl: '.hotel-slider__button--prev',
+  },
+
+// Keyboard control
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+});
+
+// ----------------------------------------------------------------------------------
+
+// Reviews Slider
+const reviewsSlider = new Swiper('.reviews-slider', {
+  // Optional parameters
+  loop: true,
+  grabCursor: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.hotel-slider__button--next',
+    prevEl: '.hotel-slider__button--prev',
   },
 
 // Keyboard control
